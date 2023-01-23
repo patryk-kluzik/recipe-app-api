3989b0c0825d9874772017875605f229d3192f9a
Patryk Kluzik <patrykkluzik99@gmail.com>
2023-01-23 16:19:29 +0000

    Add function and unit tests

---
+++ app/app/calc.py
@@ -0,0 +1,22 @@
+"""
+CALCULATOR FUNCTIONS
+"""
+
+def add(*, a: int, b: int) -> int:
+    """
+    Add numbers together
+    :param a: number to add to
+    :param b: number to add by
+    :return: sum of added numbers
+    """
+    return a + b
+
+
+def subtract(*, a: int, b: int) -> int:
+    """
+    Subtract numbers
+    :param a: number to subtract from
+    :param b: number to subtract by
+    :return: sum of subtracted numbers
+    """
+    return a - b
---
+++ app/app/tests.py
@@ -0,0 +1,31 @@
+"""
+SAMPLE TEST
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+class CalcTests(SimpleTestCase):
+    """
+    TEST CALC MODULE
+    """
+
+    def test_add_numbers(self):
+        """
+        Test adding number together.
+        :return:
+        """
+
+        res = calc.add(a=5, b=10)
+
+        self.assertEqual(res, 15)
+
+    def test_subtract_numbers(self):
+        """
+        Test subtracting numbers.
+        :return:
+        """
+
+        res = calc.subtract(a=10,b=1)
+
+        self.assertEqual(res, 9)
